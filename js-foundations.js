da8bd9e855bba98c13c1419befc97953e9173c34
Ubuntu <ubuntu@ip-172-31-16-221.us-east-2.compute.internal>
2019-08-12 15:44:32 +0000

    assessment-2

--- js-foundations.js
+++ js-foundations.js
@@ -2,12 +2,30 @@
 // Coding practical questions
 
 // 1. Write an arrow function that takes a number as an argument and decides if the number is evenly divisble by three or not. Use the test cases 15, 0, 385, -7. Expected output: "15 is divisible by three", "0 is divisible by three", "385 is not divisble by three", "-7 is not divisble by three".
+var num = 15
+var num2 = 0
+var num3 = 385
+var num4 = -7
 
+let numChecker = (number) =>{
+    if(number%3 ===0){
+     return number + " is a divisible by 3"
+     }else
+        return number + " is not divisible by 3"
+}
 
+console.log(numChecker(0))
 
 // 2. Create an object called helloMe. Include your firstName, lastName and TWO OTHER properties of you. Add a method to your object that will return a sentence using the object properties to describe you.
+var helloMe = {
+    firstName: "Jonathon",
+    lastName: "Arbona",
+    height: "6'2",
+    eyes: "blue",
+    getData: function() {return this.firstName + " " + this.lastName + " is " + this.height + " and has " + this.eyes + " eyes."}
+};
 
-
+console.log(helloMe.getData());
 
 // -------------------------------------------------
 // Consider the variable:
@@ -15,13 +33,22 @@
 var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
 
 // 3a. Write a function that returns every other item as one string. Expected output: "streetlamp teeth nephew database"
+let makeString = (arr) =>{
+    let newArr = []
+    for(let i=0; i<arr.length; i++){
+        if(i%2 === 0)
+            newArr.push(arr[i])
+    }return newArr.join(" ")
+}
 
-
+console.log(makeString(randomNouns))
 
 // 3b. Create an arrow function that takes in the variable and returns an array with the number of letters in each string. Expected output: [10, 6, 5, 10, 6, 11, 8]
+let makeNumber = (arr) =>{
+    return arr.map(value => value.length)
+}
 
-
-
+console.log(makeNumber(randomNouns))
 // 3c. Stretch: Create a function called capitalizer that takes in the variable and returns the array with all the words capitalized. Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
 
 
@@ -33,12 +60,18 @@ var testString2 = "academy"
 var testString3 = "sandiego"
 
 // 4a. Write a function called alphabetSoup that takes one argument and returns a string with all the letters in alphabetical order. Use each of the varibales as test cases. Expected output: "aelnr" "aacdemy" "adeginos"
+let alphabetSoup = (arr) =>{
+    return arr.split('').sort().join('')
+}
 
-
-
+console.log(alphabetSoup(testString1))
 // 4b. Write a function that takes in all three variables and returns a string with all the letters in alphabetical order. Expected output: "aaaacddeeegilmnnorsy"
+let allLetters = (arr, arr2, arr3) =>{
+    let newArr = arr+arr2+arr3
+    return newArr.split('').sort().join('')
+}
 
-
+console.log(allLetters(testString1, testString2, testString3))
 
 // -------------------------------------------------
 // Consider the variables:
@@ -47,6 +80,11 @@ var animals = ["ducks", "elephants", "pangolins", "zebras", "giraffes", "penguin
 
 // 5a. Write a function that takes the two variables and returns a combined string. Expected output: "9 ducks 1 elephants 8 pangolins 16 zebras 5 giraffes 1 penguins 42 llamas"
 
+let combineArr = (arr1, arr2) =>{
+    let newArr = arr1.map((value, i) => value + " " + arr2[i])
+    return newArr.join(' ')
+}
 
+console.log(combineArr(amounts, animals))
 
 // 5b. Stretch: Create a function that combines the two variables and updates the animal to be singular if the animal's corresponding number is 1. Expected output: "9 ducks 1 elephant 8 pangolins 16 zebras 5 giraffes 1 penguin 42 llamas"
--- js-foundations.md
+++ js-foundations.md
@@ -4,23 +4,23 @@
 ### From memory, answer the following:
 
 1. What's the difference between =, ==, and === in JavaScript?
-
+= is used to set a value, == is used if you want something to loosely equal something in a function and === is used for exact values.
 
 
 2. What is the difference between an array's index and length?
-
+the index is where the element is located relative to other elements, index starts at 0. While length is how long the array is starting from 1.
 
 
 3. What is a function's declaration, argument, and call?
-
+the declaration is the function it self that gets executed, argument is the values youre passing through and call is what youre trying to declare.
 
 
 4. What are the three main steps in saving work to github?
-
+git add . git commit -m and git push
 
 
 5. What is an object?
-
+an object is the combination of state and behavior
 
 
 ### Answer the following questions. First, without external resources. Challenge yourself to answer from memory. Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn.
@@ -36,59 +36,58 @@ logIt()
 
 
 1a. Look at this Javascript function and try to predict what the console show.
-
-
+undefined
 
 1b. Test the function. Explain why the function returned what it did.
 
-  Your answer:
+  Your answer: theres nothing being passed through
 
-  Researched answer:
+  Researched answer: the variable wasnt assigned in the function's scope or passed through as an argument
 
 
 2. What is JSON? How does it relate to javascript objects?
 
-  Your answer:
+  Your answer: Javascript Object Notation. it relates to objects by allowing us to give an object multiple values to call on later
 
-  Researched answer:
+  Researched answer: JSON is a lightweight format for storing and transportating data.
 
 
 3. What does CRUD stand for?
 
-  Your answer:
+  Your answer: Create Read Update Delete
 
-  Researched answer:
+  Researched answer: Create Read Update Delete
 
 
 4. What is a higher-order function?
 
-  Your answer:
+  Your answer: a higher order function is a function that uses a function for another function
 
-  Researched answer:
+  Researched answer: a function that takes a function as an argument, or returns a function 
 
 
 5. Stretch: What is a closure, what is it good for and how do you recognize one?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: Is an inner function inside an outer function. They have their own local scope and have access to outer scopes.
 
 
 6. Stretch: What is an API?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: a set of functions and procedures allowing the creation of applications that access the features or data of an operating system, application, or other service.
 
 
 ### Additional Feedback Questions.
 
 1. Do you have a suggestion for a Check In question?
-
-
+Whats your favorite meal of the day?
 
 2. What was your favorite topic this week?
-
+React State
 
 
 3. What was your "A-ha!" moment this week?
+My a-ha moment was during the pig latin challenge, when we finally got the code to work
